3839002b850d3c1b260f4072fcd657d5f21d3184
Marco Merlini <mahkoe@gmail.com>
2024-03-02 17:33:07 +0000

    Use Berkeley Star Catalogue

--- gen_ankabut.lua
+++ gen_ankabut.lua
@@ -6,26 +6,37 @@ require"fit_curve"
 require"common"
 require"constellation_lut"
 
-f = assert(io.open"IAU-CSN-fixed.txt")
-
+-----------------------
+-- Read in star data --
+-----------------------
+f = assert(io.open"bsc5.dat")
 tab = {}
 for l in f:lines() do
-    if not l:match"^#" then
-        local mag = tonumber(l:sub(82,86))
-        if mag then
-            local RA = assert(tonumber(l:sub(105,114)))
-            local DE = assert(tonumber(l:sub(116,125)))
-            local name = l:sub(1,18)
-            name = name:match("(.-)%s*$")
-            table.insert(tab, {mag, RA, DE, name})
-        end
+    local mag = tonumber(l:sub(103,107))
+
+    if mag then
+        local rah = assert(tonumber(l:sub(76,77)))
+        local ram = assert(tonumber(l:sub(78,79)))
+        local ras = assert(tonumber(l:sub(80,83)))
+        local ra_deg = 360 * (60*(60*rah+ram)+ras) / (24*60*60)
+
+        local desgn = l:sub(84,84)
+        local ded = assert(tonumber(l:sub(85,86)))
+        local dem = assert(tonumber(l:sub(87,88)))
+        local des = assert(tonumber(l:sub(89,90)))
+        local de_deg = ded + (dem + des/60)/60
+        if desgn == "-" then de_deg = -de_deg end
+
+        table.insert(tab, {mag, ra_deg, de_deg})
     end
 end
 
+table.sort(tab, function(a,b) return a[1] < b[1] end)
 f:close()
 
-table.sort(tab, function(a,b) return a[1] < b[1] end)
-
+--------------------------------
+-- SVG tag and bounding boxes --
+--------------------------------
 
 f = io.open("out/ankabut.svg", "wb")
 
@@ -45,8 +56,7 @@ f:write(string.format([[
         laser_kerf
 ))
 
--- For my own sake, and also because ponoko gets confused
--- without an outline, draw an outline
+-- For my own sake, add an outline for the safe drawing area
 f:write(string.format([[
     <rect x="%f" y="%f" width="%f" height="%f" fill="none" stroke="orange" stroke-width="%f" />
 ]],
@@ -54,7 +64,11 @@ f:write(string.format([[
     draw_width, draw_height,
     laser_kerf
 ))
--- Draw the ecliptic
+
+
+-----------------------
+-- Draw the ecliptic --
+-----------------------
 
 -- Inspired by the method in Alejandro Jenkins's (uhhh forgot the
 -- title, something like "the position of the sun"). The RA values
@@ -82,12 +96,17 @@ io.write("Working on ecliptic ")
 io.flush()
 segs = fit_function(
     ecliptic,
-    5, 365, -- fit_function has bad results when DC on first point
+    0, 360,
     draw_tol,
     true
 )
 print(", used ", #segs, "curve segments")
 f:write(segs_to_svg(segs, laser_kerf, true, "green") .. "\n")
+
+
+----------------
+-- Draw stars --
+----------------
 
 -- returns a reasonable radius for drawing a star
 -- given its magnitude
@@ -96,7 +115,7 @@ function mag_radius(mag)
     if mag > 6.5 then mag = 6.5 end
     if mag < -1 then mag = -1 end
 
-    -- Remap mag (exponentially) to range 0.5-1.5. This is a bit
+    -- Remap mag (exponentially) to range 1-3. This is a bit
     -- arbitrary, but basically I want the biggest stars
     -- to have a certain proportion relative to the 
     -- smallest, and I'm worried that anything "too small"
@@ -104,15 +123,14 @@ function mag_radius(mag)
     -- to see)
 
     mag = math.pow(10,(6.5-mag)*1/7.5) -- range 1-10
-    mag = (mag-1)*1/9 + 0.5
+    mag = (mag-1)*2/9 + 1
     
-    return laser_kerf*mag
+    return dot_size*mag
 end
 
 for i,v in ipairs(tab) do
-    break
     local mag, ra, dec, name = table.unpack(v)
-    --if mag > 6.5 then break end
+    if mag > 4.5 then break end
     --[[print(string.format(
             "RA=%10.6f DEC=% 10.6f mag=%-5.2g (%s)",
             v[2], v[3], v[1], v[4]
@@ -122,6 +140,7 @@ for i,v in ipairs(tab) do
         remap_x(ra), remap_y(dec), mag_radius(mag)
     ))
     if false and mag < 2.2 then
+        -- too bad, BSC5 doesn't have common names
         f:write(string.format(
             [[<text style="font-size:0.5" x="%f" y="%f"> %s</text>]].."\n",
             remap_x(ra), remap_y(dec), name
@@ -129,6 +148,10 @@ for i,v in ipairs(tab) do
     end
 end
 
-f:write"</svg>"
 
+------------------------
+-- Close out SVG file --
+------------------------
+
+f:write"</svg>"
 f:close()
